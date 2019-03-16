03f5ce80cc8da26f6ad116ca8d390be355b89416
aliakeel <aliakeel27@yahoo.com>
2019-03-17 00:08:54 +0330

    local db in App_Data Folder with stored procedures...

---
+++ sqlScript/insertLinkStoredProcedure.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[insertRssLink]
+	@link nvarchar(MAX)
+AS
+if not exists(SELECT COUNT(*)
+				FROM links
+				WHERE link = @link)
+begin
+	insert into links (link) values( @link)  
+end
+RETURN 0
---
+++ sqlScript/sp_insert_item.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[insertRssItem]
+	@title nvarchar(MAX),
+	@content nvarchar(MAX),
+	@date datetime,
+	@link nvarchar(MAX)
+AS
+	DECLARE @ReturnValue int
+	SELECT @ReturnValue=id FROM links WHERE link = @link
+	IF EXISTS(SELECT * FROM dbo.ItemsNews WHERE title = @title)
+	BEGIN
+		INSERT INTO dbo.ItemsNews
+		(
+		    title,
+		    content,
+			date,
+		    linkId
+		)
+		VALUES
+		(   @title, -- title - nvarchar(max)
+		    @content, -- content - nvarchar(max)
+		    GETDATE(), -- date - datetime
+		    @ReturnValue          -- linkId - int
+		    )
+	end
+RETURN 0
